3ade263f3d5e02d776ef14cf2b0048642e1d54fd
Christopher Boykin <christopherboykin@my.tridenttech.edu>
2024-11-19 18:59:51 -0500

    Add files via upload

---
+++ myFavMovie.js
@@ -0,0 +1,8 @@
+// Chris Boykin
+document.write("Shaun of the Dead<br><br>");
+document.write("In this zombie comedy Simon Pegg portrays Shaun. His mundane life consists of playing video <br>");
+document.write("games, having a pint with at the local pub with Ed, his roommate and best friend, and working at an<br>");
+document.write("electronics store. But, when the undead have taken over London Shaun decides to prove to his<br>");
+document.write("family, friends, and fed-up girlfriend that he is not a worthless bum. With all of the best<br>");
+document.write('intentions Shaun and Ed try to implement "the plan": "Take car. Go to Mums. Kill Phill.<br>');
+document.write('Grab Liz. Go to the Winchester, have a nice cold pint, and wait for all of this to blow over."<br>');
